08f5a1f1bca75ba02ef9b3cdd0ce45dbd93a637a
Mark Gottscho <mgottscho@ucla.edu>
2015-04-21 22:32:18 -0700

    Fix to windows build script

--- build-win.bat
+++ build-win.bat
@@ -2,7 +2,8 @@
 
 :: Author: Mark Gottscho <mgottscho@ucla.edu>
 
-echo Building X-Mem for Windows x86-64...
+set ARCH=%1
+echo Building X-Mem for Windows on %ARCH%...
 
 :: Do a little trick to ensure build datetime are correct
 :: DO NOT remove this code -- otherwise X-Mem will fail to build.
@@ -13,13 +14,13 @@ echo #define BUILD_DATETIME "%build_datetime%">>src\include\build_datetime.h
 echo #endif>>src\include\build_datetime.h
 
 :: Build
-call scons -f SConstruct_win_x64
+call scons -f SConstruct_win_%ARCH%
 
 :: Check if build was successful
 if ERRORLEVEL 1 goto buildFailure
 
 :: Copy executable
-copy build\win\release\xmem.exe .\xmem.exe
+copy build\win\%ARCH%\release\xmem.exe .\xmem.exe
 echo Done! The executable is at the top of the project tree: xmem.exe
 exit /B 0
 
